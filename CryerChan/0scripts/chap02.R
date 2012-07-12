03a377b7dd7bd84fca3c62fb6c60ba6edd266e4a
G. Jay Kerns <gkerns@ysu.edu>
2012-07-12 19:02:31 -0400

    added exercises

--- CryerChan/0scripts/chap02.R
+++ CryerChan/0scripts/chap02.R
@@ -1,12 +1,14 @@
+library(TSA)
+
 # Exhibit 2.1
 # rwalk contains a simulated random walk
 data(rwalk)
-plot(rwalk,type='o',ylab='Random Walk')
+plot(rwalk, type = 'o', ylab = 'Random Walk')
 
 # R code for simulating a random walk with, say 60, iid standard normal errors
-n=60
+n <- 60
 set.seed(12345) # intialize the random number so that the simulation can be 
 # reproducible.
-sim.random.walk=ts(cumsum(rnorm(n)),freq=1,start=1)
-plot(sim.random.walk,type='o',ylab='Another Random Walk')
+sim.random.walk <- ts(cumsum(rnorm(n)),freq=1,start=1)
+plot(sim.random.walk, type = 'o', ylab = 'Another Random Walk')
 
