8988dc857b43a3554bb3193cdb0f886fe264dc57
G. Jay Kerns <gkerns@ysu.edu>
2012-07-23 06:15:02 -0400

    r work

--- CryerChan/0scripts/chap08.R
+++ CryerChan/0scripts/chap08.R
@@ -1,19 +1,21 @@
 # chap8.R
 library(TSA)
+library(forecast)
+
 
 # Exhibit 8.1
 data(color)
-m1.color <- arima(color, order = c(1,0,0))
+m1.color <- Arima(color, order = c(1,0,0))
 m1.color
 plot(rstandard(m1.color), ylab = "Standardized residuals", type = "b")
 abline(h = 0)
 
 # Exhibit 8.2
 data(hare)
-m1.hare <- arima(sqrt(hare), order = c(3,0,0))
+m1.hare <- Arima(sqrt(hare), order = c(3,0,0))
 m1.hare # the AR(2) coefficient is not significant; it is second in the
 # list of coefficients.
-m2.hare <- arima(sqrt(hare), order = c(3,0,0), fixed = c(NA,0,NA,NA)) # fixed the AR(2)
+m2.hare <- Arima(sqrt(hare), order = c(3,0,0), fixed = c(NA,0,NA,NA)) # fixed the AR(2)
 # coefficient to be 0 via the fixed argument.
 m2.hare
 # Note that the intercept term is actually the mean in the centered form
@@ -26,7 +28,7 @@ abline(h = 0)
 
 # Exhibit 8.3
 data(oil.price)
-m1.oil <- arima(log(oil.price), order = c(0,1,1))
+m1.oil <- Arima(log(oil.price), order = c(0,1,1))
 plot(rstandard(m1.oil), ylab = "Standardized residuals", type = "l")
 abline(h = 0)
 
@@ -46,29 +48,30 @@ qqline(residuals(m1.oil))
 acf(residuals(m1.color), main = "Sample ACF of Residuals from AR(1) Model for Color")
 
 # Exhibit 8.10
-acf(residuals(arima(sqrt(hare), order = c(2,0,0))),
+acf(residuals(Arima(sqrt(hare), order = c(2,0,0))),
     main = "Sample ACF of Residuals from AR(2) Model for Hare")
 
 # Exhibit 8.11
 acf(residuals(m1.color), plot = FALSE)$acf
-signif(acf(residuals(m1.color), plot = FALSE)$acf[1:6],2) #  to display the first 6 acf
+signif(acf(residuals(m1.color), plot = FALSE)$acf[1:6], 2) #  to display the first 6 acf
 # to 2 significant digits.
 
 # Exhibit 8.12 
-tsdiag(m1.color, gof = 15, omit.initial = FALSE) # the tsdiag function is modified from that in the
+tsdiag(m1.color, gof = 15, omit.initial = FALSE) 
+# the tsdiag function is modified from that in the
 # stats package of R.
 
 # Exhibit 8.13
 m1.color 
 
 # Exhibit 8.14
-m2.color <- arima(color, order = c(2,0,0))
+m2.color <- Arima(color, order = c(2,0,0))
 m2.color
 
 # Exhibit 8.15
-m3.color <- arima(color, order = c(1,0,1))
+m3.color <- Arima(color, order = c(1,0,1))
 m3.color
 
 # Exhibit 8.16
-m4.color <- arima(color, order = c(2,0,1))
+m4.color <- Arima(color, order = c(2,0,1))
 m4.color
