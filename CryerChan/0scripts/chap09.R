a362415a38bccac103e8549fddf575829e1edf43
G. Jay Kerns <gkerns@ysu.edu>
2012-07-24 16:49:47 -0400

    chap 9 stuff

--- CryerChan/0scripts/chap09.R
+++ CryerChan/0scripts/chap09.R
@@ -1,10 +1,11 @@
 # chap9.R
 
 library(TSA)
+library(forecast)
 
 # Exhibit 9.1
 data(color)
-m1.color=arima(color,order=c(1,0,0))
+m1.color <- Arima(color,order=c(1,0,0))
 m1.color 
 
 # Exhibit 9.2
@@ -12,35 +13,40 @@ m1.color
 # the temperature for two years.
 data(tempdub)
 
-tempdub1=ts(c(tempdub,rep(NA,24)),start=start(tempdub),freq=frequency(tempdub)) 
+tempdub1 <- ts(c(tempdub,rep(NA,24)),
+               start = start(tempdub),
+               freq = frequency(tempdub)) 
 
 # creates the first pair of harmonic functions and then fit the model
-har.=harmonic(tempdub,1)
-m5.tempdub=arima(tempdub,order=c(0,0,0),xreg=har.)
+har. <- harmonic(tempdub,1)
+m5.tempdub <- Arima(tempdub, order = c(0,0,0), xreg = har.)
 m5.tempdub
 # The result is same as that from the fit using lm function.
-har.=harmonic(tempdub,1)
-model4=lm(tempdub~har.)
+har. <- harmonic(tempdub, 1)
+model4 <- lm(tempdub ~ har.)
 summary(model4)
 
-
 # create the harmonic functions over the period of forecast.
-newhar.=harmonic(ts(rep(1,24), start=c(1976,1),freq=12),1)
+newhar. <- harmonic(ts(rep(1,24), start = c(1976,1), freq = 12), 1)
 # Compute and plot the forecasts.
-plot(m5.tempdub,n.ahead=24,n1=c(1972,1),newxreg=newhar.,
- type="b",ylab="Temperature",xlab="Year")
+plot(m5.tempdub, n.ahead=24, n1 = c(1972,1), 
+     newxreg = newhar., type = "b",
+     ylab = "Temperature", xlab = "Year")
 
 # Exhibit 9.3 
 data(color)
-m1.color=arima(color,order=c(1,0,0))
-plot(m1.color,n.ahead=12,type="b", xlab="Time", ylab="Color Property")
+m1.color <- Arima(color, order = c(1,0,0))
+plot(m1.color, n.ahead = 12, type = "b", 
+     xlab = "Time", ylab = "Color Property")
 # add the horizontal line at the estimated mean ("intercept") 
-abline(h=coef(m1.color)[names(coef(m1.color))=="intercept"])
+abline(h = coef(m1.color)[names(coef(m1.color)) == "intercept"])
+
 
 
 # Exhibit 9.4
 data(hare)
-m1.hare=arima(sqrt(hare),order=c(3,0,0))
-plot(m1.hare, n.ahead=25,type="b",xlab="Year",ylab="Sqrt(hare)")
-abline(h=coef(m1.hare)[names(coef(m1.hare))=="intercept"])
+m1.hare <- Arima(sqrt(hare), order = c(3,0,0))
+plot(m1.hare, n.ahead = 25, type = "b", xlab = "Year",
+     ylab = "Sqrt(hare)")
+abline(h = coef(m1.hare)[names(coef(m1.hare))=="intercept"])
 
