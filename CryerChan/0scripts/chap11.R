dd34f360441a5f72eddb5978565fc5cf143c48c4
G. Jay Kerns <gkerns@ysu.edu>
2012-07-30 16:17:07 -0400

    chap 11

--- CryerChan/0scripts/chap11.R
+++ CryerChan/0scripts/chap11.R
@@ -13,7 +13,7 @@ acf(as.vector(diff(diff(window(log(airmiles),end=c(2001,8)),12))),lag.max=48)
 air.m1 <- arimax(log(airmiles), order = c(0,1,1),
                  seasonal = list(order = c(0,1,1), period = 12),
                  xtransf = data.frame(I911 = 1*(seq(airmiles) == 69),
-                                      I911=1*(seq(airmiles)==69)),
+                                      I911 = 1*(seq(airmiles) == 69)),
                  transfer = list(c(0,0), c(1,0)), 
                  xreg = data.frame(Dec96 = 1*(seq(airmiles) == 12),
                                    Jan97 = 1*(seq(airmiles) == 13),
